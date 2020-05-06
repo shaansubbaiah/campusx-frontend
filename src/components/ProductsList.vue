f59c81ffbdfb0f9659f99230259d9ff91ebc7769
shaansubbaiah <shaansubbaiah.cs18@bmsce.ac.in>
2020-05-06 21:04:22 +0530

    Fix filter, other card layouts

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -15,35 +15,48 @@
           </md-field>
         </md-toolbar>
 
-        <md-button v-on:click="retrieveProducts" class="md-dense md-raised">FILTER</md-button>
-        <md-button v-on:click="clearSearch" class="md-dense md-raised">CLEAR</md-button>
-
+        <!-- 
+        Filters
+        -->
         <div class="md-layout-item md-layout md-gutter" id="filters">
-          <md-field class="md-layout-item" id="sem-filter">
-            <label for="sem">Semester</label>
-            <md-select v-model="ssem" name="sem" id="sem">
-              <md-option
-                v-for="semester in semesters"
-                :value="semester"
-                :key="semester"
-              >{{semester}}</md-option>
-            </md-select>
-          </md-field>
+          <div class="md-layout-item">
+            <md-field id="sem-filter">
+              <label for="sem">Semester</label>
+              <md-select v-model="ssem" name="sem" id="sem">
+                <md-option
+                  v-for="semester in semesters"
+                  :value="semester"
+                  :key="semester"
+                >{{semester}}</md-option>
+              </md-select>
+            </md-field>
+          </div>
 
-          <md-field class="md-layout-item" id="branch-filter">
-            <label for="branch">Branch</label>
-            <md-select v-model="sbranch" name="branch" id="branch">
-              <md-option v-for="branch in branches" :value="branch" :key="branch">{{branch}}</md-option>
-            </md-select>
-          </md-field>
+          <div class="md-layout-item">
+            <md-field id="branch-filter">
+              <label for="branch">Branch</label>
+              <md-select v-model="sbranch" name="branch" id="branch">
+                <md-option v-for="branch in branches" :value="branch" :key="branch">{{branch}}</md-option>
+              </md-select>
+            </md-field>
+          </div>
 
-          <md-field class="md-layout-item" id="donation-filter">
-            <label for="donation">Type</label>
-            <md-select v-model="sdonation" name="donation" id="donation">
-              <md-option value="1">Donation</md-option>
-              <md-option value="0">Trade</md-option>
-            </md-select>
-          </md-field>
+          <div class="md-layout-item">
+            <md-field id="donation-filter">
+              <label for="donation">Type</label>
+              <md-select v-model="sdonation" name="donation" id="donation">
+                <md-option value="1">Donation</md-option>
+                <md-option value="0">Trade</md-option>
+              </md-select>
+            </md-field>
+          </div>
+          <!-- 
+          Filter Buttons 
+          -->
+          <div class="md-layout-item" id="filter-buttons">
+            <md-button v-on:click="retrieveProducts" class="md-primary">FILTER</md-button>
+            <md-button v-on:click="clearSearch" class="md-accent">CLEAR</md-button>
+          </div>
         </div>
       </div>
     </div>
@@ -106,57 +119,93 @@
         Drive card layout
         -->
         <div v-if="product.drive">
-          <div class="md-layout-item" id="cards">
-            <a :href="'/product/' + product.id">
-              <md-card md-with-hover>
-                <md-ripple>
-                  <md-card-header>
-                    <div class="md-title">{{product.title}}</div>
-                    <div class="md-subhead">{{product.drive.description}}</div>
-                  </md-card-header>
+          <md-card md-with-hover>
+            <md-ripple>
+              <md-card-header>
+                <a :href="'/product/' + product.id">
+                  <div class="md-title">{{product.title}}</div>
+                  <div class="md-subhead">by {{product.drive.description}}</div>
+                </a>
+              </md-card-header>
 
-                  <md-card-expand>
-                    <md-card-actions md-alignment="space-between">
-                      <div>
-                        <md-chip class="md-primary" md-clickable>{{product.sem}}</md-chip>
-                        <md-chip class="md-primary" md-clickable>{{product.branch}}</md-chip>
-                      </div>
-                    </md-card-actions>
-                  </md-card-expand>
-                </md-ripple>
-              </md-card>
-            </a>
-          </div>
+              <md-card-expand>
+                <md-card-actions md-alignment="space-between">
+                  <div>
+                    <md-chip>{{product.sem}}</md-chip>
+                    <md-chip>{{product.branch}}</md-chip>
+                  </div>
+                  <md-card-expand-trigger>
+                    <md-button class="md-icon-button">
+                      <md-icon>keyboard_arrow_down</md-icon>
+                    </md-button>
+                  </md-card-expand-trigger>
+                </md-card-actions>
+
+                <md-card-expand-content>
+                  <md-card-content>
+                    Links to: {{product.drive.url}}
+                    <br />
+                    Posted at: {{product.createdAt}}
+                    <br />
+                  </md-card-content>
+                </md-card-expand-content>
+              </md-card-expand>
+            </md-ripple>
+          </md-card>
         </div>
 
+        <!-- 
+        Other card layout
+        -->
         <div v-if="product.other">
-          <div class="md-layout-item" id="cards">
-            <a :href="'/product/' + product.id">
-              <md-card md-with-hover>
-                <md-ripple>
-                  <md-card-header>
-                    <div class="md-title">{{product.title}}</div>
-                    <div class="md-subhead">{{product.other.description}}</div>
-                  </md-card-header>
+          <md-card md-with-hover>
+            <md-ripple>
+              <md-card-media>
+                <a :href="'/product/' + product.id">
+                  <img
+                    v-bind:src="'http://localhost:8080/'+product.other.image"
+                    alt="product.title"
+                  />
+                </a>
+              </md-card-media>
 
-                  <md-card-expand>
-                    <md-card-actions md-alignment="space-between">
-                      <div>
-                        <md-chip class="md-primary" md-clickable>{{product.sem}}</md-chip>
-                        <md-chip class="md-primary" md-clickable>{{product.branch}}</md-chip>
-                        <div v-if="product.donation">
-                          <md-chip class="md-primary" id="donation" md-clickable>DONATION</md-chip>
-                        </div>
-                      </div>
-                    </md-card-actions>
-                  </md-card-expand>
-                </md-ripple>
-              </md-card>
-            </a>
-          </div>
+              <md-card-header>
+                <div class="md-title">{{product.title}}</div>
+                <div class="md-subhead">{{product.other.description}}</div>
+              </md-card-header>
+
+              <md-card-expand>
+                <md-card-actions md-alignment="space-between">
+                  <div>
+                    <md-chip>{{product.sem}}</md-chip>
+                    <md-chip>{{product.branch}}</md-chip>
+                    <div v-if="product.donation">
+                      <md-chip>D</md-chip>
+                    </div>
+                  </div>
+                  <md-card-expand-trigger>
+                    <md-button class="md-icon-button">
+                      <md-icon>keyboard_arrow_down</md-icon>
+                    </md-button>
+                  </md-card-expand-trigger>
+                </md-card-actions>
+
+                <md-card-expand-content>
+                  <md-card-content>
+                    Contact: {{product.other.phone}}
+                    <br />
+                    Posted at: {{product.createdAt}}
+                    <br />
+                  </md-card-content>
+                </md-card-expand-content>
+              </md-card-expand>
+            </md-ripple>
+          </md-card>
         </div>
       </div>
     </div>
+
+    <!-- End card layouts -->
   </div>
 </template>
  
@@ -231,15 +280,25 @@ export default {
 }
 
 .md-card {
-  width: 320px;
+  width: 280px;
   margin: 4px;
   display: inline-block;
   vertical-align: top;
-  border-radius: 20px;
+  border-radius: 20px !important;
+}
+
+.md-card-media {
+  border-radius: 20px !important;
 }
 
 .md-card-expand {
   border-radius: 0px 0px 20px 20px;
+}
+
+#filter-buttons {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 }
 /* #search-bar {
   margin: auto;
