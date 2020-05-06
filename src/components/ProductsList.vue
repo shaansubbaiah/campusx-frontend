2815f22b46d44ec303e2949d95e00695c7ef1adc
shaansubbaiah <shaansubbaiah.cs18@bmsce.ac.in>
2020-05-06 23:43:43 +0530

    Add optional card search, fancify search

--- src/components/ProductsList.vue
+++ src/components/ProductsList.vue
@@ -3,9 +3,9 @@
     <!-- 
     Filters, search bar
     -->
-    <div class="md-layout">
-      <div class="search">
-        <md-toolbar class="md-transparent" id="search-bar" md-elevation="0">
+    <div>
+      <div>
+        <md-toolbar class="md-medium md-transparent" id="search-bar" md-elevation="20">
           <md-field>
             <label for="title">Search..</label>
             <md-input name="title" id="title" v-model="stitle"></md-input>
@@ -14,6 +14,78 @@
             </md-button>
           </md-field>
         </md-toolbar>
+
+        <md-card class="search-card">
+          <md-card-header>
+            <md-field>
+              <label for="title">Search..</label>
+              <md-input name="title" id="title" v-model="stitle"></md-input>
+              <md-button v-on:click="retrieveProducts" class="md-icon-button" id="search-icon">
+                <md-icon>search</md-icon>
+              </md-button>
+            </md-field>
+          </md-card-header>
+
+          <md-card-expand>
+            <md-card-actions md-alignment="space-between">
+              filter
+              <md-card-expand-trigger>
+                <md-button class="md-icon-button">
+                  <md-icon>keyboard_arrow_down</md-icon>
+                </md-button>
+              </md-card-expand-trigger>
+            </md-card-actions>
+
+            <md-card-expand-content>
+              <md-card-content>
+                <div class="md-layout-item md-layout md-gutter" id="filters">
+                  <div class="md-layout-item">
+                    <md-field id="sem-filter">
+                      <label for="sem">Semester</label>
+                      <md-select v-model="ssem" name="sem" id="sem">
+                        <md-option
+                          v-for="semester in semesters"
+                          :value="semester"
+                          :key="semester"
+                        >{{semester}}</md-option>
+                      </md-select>
+                    </md-field>
+                  </div>
+
+                  <div class="md-layout-item">
+                    <md-field id="branch-filter">
+                      <label for="branch">Branch</label>
+                      <md-select v-model="sbranch" name="branch" id="branch">
+                        <md-option
+                          v-for="branch in branches"
+                          :value="branch"
+                          :key="branch"
+                        >{{branch}}</md-option>
+                      </md-select>
+                    </md-field>
+                  </div>
+
+                  <div class="md-layout-item">
+                    <md-field id="donation-filter">
+                      <label for="donation">Type</label>
+                      <md-select v-model="sdonation" name="donation" id="donation">
+                        <md-option value="1">Donation</md-option>
+                        <md-option value="0">Trade</md-option>
+                      </md-select>
+                    </md-field>
+                  </div>
+                  <!-- 
+          Filter Buttons 
+                  -->
+                  <div class="md-layout-item" id="filter-buttons">
+                    <md-button v-on:click="retrieveProducts" class="md-primary">FILTER</md-button>
+                    <md-button v-on:click="clearSearch" class="md-accent">CLEAR</md-button>
+                  </div>
+                </div>
+              </md-card-content>
+            </md-card-expand-content>
+          </md-card-expand>
+        </md-card>
 
         <!-- 
         Filters
@@ -71,55 +143,60 @@
         Book card layout
         -->
         <div v-if="product.book">
-          <md-card md-with-hover>
-            <md-ripple>
-              <md-card-media>
-                <a :href="'/product/' + product.id">
-                  <img v-bind:src="'http://localhost:8080/'+product.book.image" alt="product.title" />
-                </a>
-              </md-card-media>
+          <transition name="fade">
+            <md-card class="thing-card">
+              <md-ripple>
+                <md-card-media>
+                  <a :href="'/product/' + product.id">
+                    <img
+                      v-bind:src="'http://localhost:8080/'+product.book.image"
+                      alt="product.title"
+                    />
+                  </a>
+                </md-card-media>
 
-              <md-card-header>
-                <div class="md-title">
-                  {{product.title}}
-                  <md-chip class="donation-chip md-primary" v-if="product.donation">D</md-chip>
-                </div>
-                <div class="md-subhead">by {{product.book.author}}</div>
-              </md-card-header>
-
-              <md-card-expand>
-                <md-card-actions md-alignment="space-between">
-                  <div>
-                    <md-chip>{{product.sem}}</md-chip>
-                    <md-chip>{{product.branch}}</md-chip>
+                <md-card-header>
+                  <div class="md-title">
+                    {{product.title}}
+                    <md-chip class="donation-chip md-primary" v-if="product.donation">D</md-chip>
                   </div>
-                  <md-card-expand-trigger>
-                    <md-button class="md-icon-button">
-                      <md-icon>keyboard_arrow_down</md-icon>
-                    </md-button>
-                  </md-card-expand-trigger>
-                </md-card-actions>
+                  <div class="md-subhead">by {{product.book.author}}</div>
+                </md-card-header>
 
-                <md-card-expand-content>
-                  <md-card-content>
-                    Published by {{product.book.publisher}}
-                    <br />
-                    Contact: {{product.book.phone}}
-                    <br />
-                    Posted at: {{product.createdAt}}
-                    <br />
-                  </md-card-content>
-                </md-card-expand-content>
-              </md-card-expand>
-            </md-ripple>
-          </md-card>
+                <md-card-expand>
+                  <md-card-actions md-alignment="space-between">
+                    <div>
+                      <md-chip>{{product.sem}}</md-chip>
+                      <md-chip>{{product.branch}}</md-chip>
+                    </div>
+                    <md-card-expand-trigger>
+                      <md-button class="md-icon-button">
+                        <md-icon>keyboard_arrow_down</md-icon>
+                      </md-button>
+                    </md-card-expand-trigger>
+                  </md-card-actions>
+
+                  <md-card-expand-content>
+                    <md-card-content>
+                      Published by {{product.book.publisher}}
+                      <br />
+                      Contact: {{product.book.phone}}
+                      <br />
+                      Posted at: {{product.createdAt}}
+                      <br />
+                    </md-card-content>
+                  </md-card-expand-content>
+                </md-card-expand>
+              </md-ripple>
+            </md-card>
+          </transition>
         </div>
 
         <!-- 
         Drive card layout
         -->
         <div v-if="product.drive">
-          <md-card md-with-hover>
+          <md-card class="thing-card">
             <md-ripple>
               <md-card-header>
                 <a :href="'/product/' + product.id">
@@ -158,7 +235,7 @@
         Other card layout
         -->
         <div v-if="product.other">
-          <md-card md-with-hover>
+          <md-card class="thing-card">
             <md-ripple>
               <md-card-media>
                 <a :href="'/product/' + product.id">
@@ -280,19 +357,37 @@ export default {
   height: 480px;
 }
 
+/* .search-card {
+  width: 640px;
+} */
+
+.md-toolbar {
+  border: 4px solid rgba(0, 119, 255, 0.479) !important;
+  box-shadow: 0 7px 45px -10px rgba(0, 119, 255, 0.479) !important;
+  border-radius: 30px !important;
+  margin: 25px !important;
+}
 .md-card {
   width: 280px;
   margin: 4px;
   display: inline-block;
   vertical-align: top;
+  box-shadow: 0 7px 30px -10px rgba(150, 170, 180, 0.6) !important;
+}
+
+.md-card:hover {
+  transform: scale(1.02);
+  transition-duration: 1s !important;
+  box-shadow: 0 7px 45px -10px rgba(150, 170, 180, 0.7) !important;
+}
+
+img,
+.thing-card,
+.thing-card .md-card-media {
   border-radius: 20px !important;
 }
 
-.md-card-media {
-  border-radius: 20px !important;
-}
-
-.md-card-expand {
+.thing-card .md-card-expand {
   border-radius: 0px 0px 20px 20px;
 }
 
@@ -304,9 +399,5 @@ export default {
 
 .donation-chip {
   float: right;
-}
-
-img {
-  border-radius: 20px !important;
 }
 </style>
