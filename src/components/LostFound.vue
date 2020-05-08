d07daecb57914d4696c532b66b8ab61de5a89fd8
shaansubbaiah <shaansubbaiah.cs18@bmsce.ac.in>
2020-05-08 12:48:56 +0530

    Add lostfound html, css. But js pending

--- src/components/LostFound.vue
+++ src/components/LostFound.vue
@@ -1,10 +1,50 @@
 <template>
-  <div>Lost Found</div>
+  <div>
+    <div id="container">
+      <div v-for="(LostFound,index) in lostfounds" :key="index">
+        <!-- 
+        LostFound card layout
+        -->
+        <md-card>
+          <md-ripple>
+            <md-card-media>
+              <img v-bind:src="'http://localhost:8080/'+product.book.image" alt="lostfound.title" />
+            </md-card-media>
+
+            <md-card-header>
+              <div class="md-title">{{lostfound.title}}</div>
+              <div class="md-subhead">found at {{lostfound.location}}</div>
+            </md-card-header>
+
+            <md-card-expand>
+              <md-card-actions md-alignment="space-between">
+                <div>
+                  <md-chip>{{lostfound.phone}}</md-chip>
+                </div>
+                <md-card-expand-trigger>
+                  <md-button class="md-icon-button">
+                    <md-icon>keyboard_arrow_down</md-icon>
+                  </md-button>
+                </md-card-expand-trigger>
+              </md-card-actions>
+
+              <md-card-expand-content>
+                <md-card-content>
+                  Located at: {{lostfound.description}}
+                  <br />
+                  Posted at: {{lostfound.createdAt}}
+                  <br />
+                </md-card-content>
+              </md-card-expand-content>
+            </md-card-expand>
+          </md-ripple>
+        </md-card>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import http from "../http-common";
-
 export default {
   name: "LostFound",
   data() {
