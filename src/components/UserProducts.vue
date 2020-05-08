edf6087ba6b1045d557b46ea69b6ddbd1cb77e9a
shaansubbaiah <shaansubbaiah.cs18@bmsce.ac.in>
2020-05-08 13:54:06 +0530

    Style user products

--- src/components/UserProducts.vue
+++ src/components/UserProducts.vue
@@ -2,11 +2,15 @@
   <div class="card-expansion">
     <div class="container">
       <div v-for="(product,index) in products" :key="index">
-        <!-- 
+        <div class="card-button-container">
+          <div class="button-box">
+            <md-button class="md-accent" v-on:click="deleteProduct(product.id)">Delete</md-button>
+            <md-button class="md-primary" :href="'/update/' + product.id">Edit</md-button>
+          </div>
+          <!-- 
         Book card layout
-        -->
-        <div v-if="product.book">
-          <transition name="fade">
+          -->
+          <div v-if="product.book">
             <md-card class="rounded-card">
               <md-ripple>
                 <md-card-media>
@@ -52,96 +56,96 @@
                 </md-card-expand>
               </md-ripple>
             </md-card>
-          </transition>
-        </div>
+          </div>
 
-        <!-- 
+          <!-- 
         Drive card layout
-        -->
-        <div v-if="product.drive">
-          <md-card class="rounded-card">
-            <md-ripple>
-              <md-card-header>
-                <a :href="'/product/' + product.id">
-                  <div class="md-title">{{product.title}}</div>
-                  <div class="md-subhead">by {{product.drive.description}}</div>
-                </a>
-              </md-card-header>
+          -->
+          <div v-if="product.drive">
+            <md-card class="rounded-card">
+              <md-ripple>
+                <md-card-header>
+                  <a :href="'/product/' + product.id">
+                    <div class="md-title">{{product.title}}</div>
+                    <div class="md-subhead">by {{product.drive.description}}</div>
+                  </a>
+                </md-card-header>
 
-              <md-card-expand>
-                <md-card-actions md-alignment="space-between">
-                  <div>
-                    <md-chip>{{product.sem}}</md-chip>
-                    <md-chip>{{product.branch}}</md-chip>
-                  </div>
-                  <md-card-expand-trigger>
-                    <md-button class="md-icon-button">
-                      <md-icon>keyboard_arrow_down</md-icon>
-                    </md-button>
-                  </md-card-expand-trigger>
-                </md-card-actions>
+                <md-card-expand>
+                  <md-card-actions md-alignment="space-between">
+                    <div>
+                      <md-chip>{{product.sem}}</md-chip>
+                      <md-chip>{{product.branch}}</md-chip>
+                    </div>
+                    <md-card-expand-trigger>
+                      <md-button class="md-icon-button">
+                        <md-icon>keyboard_arrow_down</md-icon>
+                      </md-button>
+                    </md-card-expand-trigger>
+                  </md-card-actions>
 
-                <md-card-expand-content>
-                  <md-card-content>
-                    Links to: {{product.drive.url}}
-                    <br />
-                    Posted at: {{product.createdAt}}
-                    <br />
-                  </md-card-content>
-                </md-card-expand-content>
-              </md-card-expand>
-            </md-ripple>
-          </md-card>
-        </div>
+                  <md-card-expand-content>
+                    <md-card-content>
+                      Links to: {{product.drive.url}}
+                      <br />
+                      Posted at: {{product.createdAt}}
+                      <br />
+                    </md-card-content>
+                  </md-card-expand-content>
+                </md-card-expand>
+              </md-ripple>
+            </md-card>
+          </div>
 
-        <!-- 
+          <!-- 
         Other card layout
-        -->
-        <div v-if="product.other">
-          <md-card class="rounded-card">
-            <md-ripple>
-              <md-card-media>
-                <a :href="'/product/' + product.id">
-                  <img
-                    v-bind:src="'http://localhost:8080/'+product.other.image"
-                    alt="product.title"
-                  />
-                </a>
-              </md-card-media>
+          -->
+          <div v-if="product.other">
+            <md-card class="rounded-card">
+              <md-ripple>
+                <md-card-media>
+                  <a :href="'/product/' + product.id">
+                    <img
+                      v-bind:src="'http://localhost:8080/'+product.other.image"
+                      alt="product.title"
+                    />
+                  </a>
+                </md-card-media>
 
-              <md-card-header>
-                <div class="md-title">
-                  {{product.title}}
-                  <md-chip class="donation-chip md-primary" v-if="product.donation">D</md-chip>
-                </div>
-
-                <div class="md-subhead">{{product.other.description}}</div>
-              </md-card-header>
-
-              <md-card-expand>
-                <md-card-actions md-alignment="space-between">
-                  <div>
-                    <md-chip>{{product.sem}}</md-chip>
-                    <md-chip>{{product.branch}}</md-chip>
+                <md-card-header>
+                  <div class="md-title">
+                    {{product.title}}
+                    <md-chip class="donation-chip md-primary" v-if="product.donation">D</md-chip>
                   </div>
-                  <md-card-expand-trigger>
-                    <md-button class="md-icon-button">
-                      <md-icon>keyboard_arrow_down</md-icon>
-                    </md-button>
-                  </md-card-expand-trigger>
-                </md-card-actions>
 
-                <md-card-expand-content>
-                  <md-card-content>
-                    Contact: {{product.other.phone}}
-                    <br />
-                    Posted at: {{product.createdAt}}
-                    <br />
-                  </md-card-content>
-                </md-card-expand-content>
-              </md-card-expand>
-            </md-ripple>
-          </md-card>
+                  <div class="md-subhead">{{product.other.description}}</div>
+                </md-card-header>
+
+                <md-card-expand>
+                  <md-card-actions md-alignment="space-between">
+                    <div>
+                      <md-chip>{{product.sem}}</md-chip>
+                      <md-chip>{{product.branch}}</md-chip>
+                    </div>
+                    <md-card-expand-trigger>
+                      <md-button class="md-icon-button">
+                        <md-icon>keyboard_arrow_down</md-icon>
+                      </md-button>
+                    </md-card-expand-trigger>
+                  </md-card-actions>
+
+                  <md-card-expand-content>
+                    <md-card-content>
+                      Contact: {{product.other.phone}}
+                      <br />
+                      Posted at: {{product.createdAt}}
+                      <br />
+                    </md-card-content>
+                  </md-card-expand-content>
+                </md-card-expand>
+              </md-ripple>
+            </md-card>
+          </div>
         </div>
       </div>
     </div>
@@ -199,4 +203,28 @@ export default {
 </script> 
 
 <style scoped>
+.button-box {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.card-button-container {
+  display: block;
+  border-radius: 25px;
+  box-shadow: 0 7px 45px -10px rgba(68, 138, 255, 0.6);
+  background: rgb(255, 255, 255);
+  background: linear-gradient(
+    180deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(68, 138, 255, 1) 100%
+  );
+  margin: 10px 5px 10px 5px;
+}
+/* .card-button-container:hover {
+  transform: scale(0.99);
+  transition-duration: 0.2s !important;
+  box-shadow: 0 7px 45px -10px rgba(150, 170, 180, 0.7) !important;
+} */
 </style>
